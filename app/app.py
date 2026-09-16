7662fd3a22e173c0a8cb569c333a43304205ed11
agustinmend <jose.mendoza.u1@ucb.edu.bo>
2026-09-16 09:05:50 -0400

    actualizacion sprint3

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import uuid
 from models.customer import Customer
 from models.table import Tables
 from models.reservation import Reservations
-from flask import Flask, render_template, jsonify, abort, request
+from flask import Flask, render_template, jsonify, abort, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import UUID
 from config.db import db
@@ -11,7 +11,7 @@ import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://app:123qwe@db:5432/restaurant_database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://app:123qwe@localhost:5432/restaurant_database'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
@@ -23,6 +23,30 @@ def get_customers():
         'total': len(customers),
         'clientes': [c.format() for c in customers]
     })
+#EJERCICIO EN CLASER GET, POST, DELETE, PATCH EN FORMULARIO
+@app.route('/formulario', methods=['GET'])
+def get_form():
+    return render_template('customers/formulario.html')
+
+@app.route('/customer/crear', methods=['POST'])
+def create_customer():
+    try:
+        customer = Customer(
+            first_name = request.form['first_name'],
+            last_name = request.form['last_name'],
+            email = request.form['email'],
+            phone = request.form['phone']
+        )
+        db.session.add(customer)
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+
+    return redirect(url_for('get_customer_by_id', person_id=customer.id))
+
+
+
+#FIN
 
 @app.route('/customers/<uuid:person_id>', methods=['GET'])
 def get_customer_by_id(person_id):
